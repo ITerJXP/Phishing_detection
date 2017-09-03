ef28ae75e525ca9354af09f6a5e64dde662f7358
ITerJXP <jxptaylover@163.com>
2017-09-03 18:48:38 +0800

    Update get_whois.py

--- get_whois.py
+++ get_whois.py
@@ -13,8 +13,18 @@ def get_whois_info(url):
     for k,v in whois_info.items():
         print '{}:{}'.format(k,v)
 
+def get_whois_s_info(url, key):
+    whois_info = whois.whois(url)
+    print type(whois_info[key]), whois_info[key]
 
+        
+        
 if __name__ == '__main__':
-    url = sys.argv[1]  
+    url = sys.argv[1]
+    key = sys.argv[2]
+   
     if len(sys.argv) == 2:
         get_whois_info(url)
+
+    if key!=None and len(sys.argv) == 3:
+       get_whois_s_info(url, key)
